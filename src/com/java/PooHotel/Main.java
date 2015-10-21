dd690ff5d31715ae7868b3c5d0e926adf8163707
Rodrigo <rodrigoferreira001@hotmail.com>
2015-10-20 22:03:37 -0200

    OOO BAGULHO EHH LOCO MESSSMO

--- src/com/java/PooHotel/Main.java
+++ src/com/java/PooHotel/Main.java
@@ -1,6 +1,7 @@
 package com.java.PooHotel;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -24,7 +25,7 @@ public class Main {
 			
 			switch(op){
 			case 1:
-				
+				checkIn();
 				
 				break;
 				
@@ -70,6 +71,14 @@ public class Main {
 		int op = 0;
 		int codCliente;
 		int codQuarto;
+		Reserva r;
+		Date entrada, saida;
+		int diaEntrada, mesEntrada, anoEntrada;
+		int diaSaida, mesSaida, anoSaida;
+		boolean flg1 = false;
+		boolean flg2 = false;
+		
+		//entrada = new Date
 		
 		System.out.println("1 - Cliente cadastrado");
 		System.out.println("2 - Novo cliente");
@@ -83,27 +92,86 @@ public class Main {
 		case 2:
 			codCliente = cadastrarCliente();
 			
-			System.out.println("Qual tipo de quarto deseja?");
-			System.out.println("Categoria			|	Valor da diária	| Capacidade");
-			System.out.println("1) Standard			|	R$ 268,00		|	2");
-			System.out.println("2) Vista Bosque		|	R$ 315,00		|	4");
-			System.out.println("3) Vista Vale		|	R$ 353,00		|	4");
-			System.out.println("4) Suíte			|	R$ 498,00		|	2");
+			System.out.println("Qual data deseja realizar a reserva?");
+			System.out.println("Dia:");
+			diaEntrada = sc.nextInt();
+			System.out.println("Mês:");
+			mesEntrada = sc.nextInt();
+			System.out.println("Ano:");
+			anoEntrada = sc.nextInt();
 			
-			codQuarto = sc.nextInt();
+			entrada = new Date((anoEntrada - 1900), (mesEntrada - 1), diaEntrada);
 			
-			if(codQuarto > 0 && codQuarto < 5){
+			System.out.println("Até quando deseja ficar?");
+			System.out.println("Dia:");
+			diaSaida = sc.nextInt();
+			System.out.println("Mês:");
+			mesSaida = sc.nextInt();
+			System.out.println("Ano:");
+			anoSaida = sc.nextInt();
+			
+			saida = new Date((anoSaida - 1900), (mesSaida - 1), diaSaida);
+			
+			if(entrada.before(saida)){
 				
-				for(int i = 0; i < 10; i++){
-					if(quartos[i].getTipo() == codQuarto){
-						//Zhuuu-liii, do the thing!
-					}
-				}
+				System.out.println("Qual tipo de quarto deseja?");
+				System.out.println("Categoria			|	Valor da diária	| Capacidade");
+				System.out.println("1) Standard			|	R$ 268,00		|	2");
+				System.out.println("2) Vista Bosque		|	R$ 315,00		|	4");
+				System.out.println("3) Vista Vale		|	R$ 353,00		|	4");
+				System.out.println("4) Suíte			|	R$ 498,00		|	2");
+				
+				codQuarto = sc.nextInt();
+				
+				//Verifica se o tipo de quarto escolhido é válido
+				if(codQuarto > 0 && codQuarto < 5){
 					
+					//Procura um quarto que seja do tipo escolhido
+					for(int i = 0; i < 10; i++){
+					
+						if(quartos[i].getTipo() == codQuarto){
+							
+							if(quartos[i].isOcupado()){
+								
+								//Procura nas reservas por um check-out menor que o check-in fornecido
+								for(int j = 0; j < reservas.size(); j++){
+									if(reservas.get(j).getNumQuarto() == codQuarto){
+										
+										if(!flg1){
+											if(reservas.get(j).getDataSaida().before(entrada)){
+												//Possível realizar check-in
+												flg1 = true;
+											}
+										}else{
+											if(reservas.get(j).getDataSaida().after(saida)){
+												//Possível realizar check-in
+												flg2 = true;
+												break;												
+											}
+										}
+										
+									}
+								}
+							}else{
+								//Quarto disponível
+							}
+						}
+						if(flg2){
+							break;
+						}
+					}
+						
+				}else{
+					System.out.println("Opção inválida!");
+				}
+				
+				//r = new Reserva();
+
+				
 			}else{
-				System.out.println("Opção inválida!");
+				System.out.println("A data de entrada deve ser menor que a data de saída!");
 			}
-			
+						
 			break;
 			
 		default:
